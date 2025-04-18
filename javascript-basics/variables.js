caa8654226879b924cd519a54bb342b7d73a56de
Arjun_Gupta <gupta.arjun.8124@gmail.com>
2025-04-18 17:42:57 +0530

    variables basics in js

--- javascript-basics/variables.js
+++ javascript-basics/variables.js
@@ -1,12 +1,14 @@
 const accountId = 123456;
-let accountEmail = "gupta.arjun@google.com";
-var accountPassword = "12345";
-city = "delhi";
+let accountEmail;
+// always use let.
+// var accountPassword = "12345";
+// var ek number ka harami hota h kyuki scope mei problems le aata h never use var
+let city;
 
 // accountId = 123;
 // console.log(accountId); not allowed to change const variables
 accountEmail= "arjun@gmail.com";
-accountPassword = "123"
+// accountPassword = "123"
 city = "chennai";
 console.log(accountEmail);
-console.table([accountEmail,accountPassword,accountId,city]);
+console.table([accountEmail,accountId,city]);
