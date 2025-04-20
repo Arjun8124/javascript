20752e5ab77b12c8ec04ed0d60099b36259dfa71
Arjun_Gupta <gupta.arjun.8124@gmail.com>
2025-04-20 18:46:31 +0530

    conversion basics

---
+++ javascript-basics/conversion.js
@@ -0,0 +1,18 @@
+let a = undefined;
+console.log(typeof a);
+
+let b = Number(a)
+console.log(typeof b);
+console.log(b);
+
+//"30" => 30 easy
+//"30abc" => NaN(not a number)
+//"" => 0
+//null => 0
+//undefined => NaN
+
+let c = undefined;
+let d = Boolean(c);
+console.log(d)
+
+//anything except 0, "", null, undefined, NaN is true
